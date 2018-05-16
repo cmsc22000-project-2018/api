e4d023e43af7641bb5c8e3ce09ac31ea0f89b656
CS Student <yjyun0922@uchicago.edu>
2018-05-15 20:17:14 -0500

    committing

--- functions_YJ/fun.c
+++ functions_YJ/fun.c
@@ -4,6 +4,40 @@
 
 #include "fun.h"
 
+zset_t* zset_new(char *name)
+{
+    zset_t *zset;
+    int rc;
+
+    zset = malloc(sizeof(zset_t));
+
+    if (zset == NULL)
+    {
+        printf("zset_new: could not allocate memory\n");
+        return NULL;
+    }
+
+    rc = zset_init(zset, name);
+
+    if (rc != 0)
+    {
+        printf("zset_new: could not initialize zset\n");
+        return NULL;
+    }
+
+    return zset;
+}
+
+int zset_init(zset_t *zset, char *name)
+{
+    assert(zset != NULL);
+
+    zset->name = name;
+    zset->context = NULL;
+
+    return 0;
+}
+
 //Alan wrote these two functions below for connecting to the server
 /*
  * connect - establishes a connection to a Redis server
@@ -45,6 +79,34 @@ int connected(zset_t *z)
     if (z)
         return z->context != NULL;
     return 0;
+}
+
+int zset_free(zset_t *zset)
+{
+    assert(zset != NULL);
+
+    redisFree(zset->context);
+    free(zset);
+
+    return 0;
+}
+
+int zset_add(zset_t *z, char *key, int score)
+{
+    if (!connected(z))
+        z->context = apiConnect("127.0.0.1", 6379); //localhost
+
+    redisReply *reply = redisCommand(z->context, "ZADD %s %d %s", z->name, score, key);
+
+    if (reply == NULL) {
+        printf("ERROR: %s\n", reply->str);
+        freeReplyObject(reply);
+        return 0;
+    }
+
+    printf("ZADD: %s\n", reply->str);
+    freeReplyObject(reply);
+    return 1;
 }
 
 int get_rank(zset_t* z, char* key)
--- functions_YJ/main.c
+++ functions_YJ/main.c
@@ -6,8 +6,8 @@
 int main() {
 	zset_t *test_zset = zset_new("fruits"); 
 	zset_add(test_zset, "bananas", 15); 
-	get_rank(test_zset, "15"); 
-	how_many_members(test_zset); 	
+	//get_rank(test_zset, "15"); 
+	//how_many_members(test_zset); 	
 	return 0; 
 }
 
